29020cd3cbf200ca708e105dac7a2121bc16e3e7
Richard Foster <richard@rpf.me>
2020-06-09 03:43:03 +0100

    kinda modelled

---
+++ modules/chess/index.test.js
@@ -0,0 +1,1 @@
+const Chess = require("./")
--- public/script.js
+++ public/script.js
@@ -1,9 +1,10 @@
-import THREE from "https://dev.jspm.io/three"
-import { GLTFLoader } from "https://dev.jspm.io/three/examples/jsm/loaders/GLTFLoader"
+import THREE from "https://dev.jspm.io/npm:three@0.117.1"
+import { GLTFLoader } from "https://dev.jspm.io/npm:three@0.117.1/examples/jsm/loaders/GLTFLoader"
+import { OrbitControls } from "https://dev.jspm.io/npm:three@0.117.1/examples/jsm/controls/OrbitControls"
 
-const FOV = 75
-const WIDTH = Math.min(window.innerWidth, 900)
-const HEIGHT = Math.min(window.innerHeight, 700)
+const FOV = 60
+const WIDTH = window.innerWidth
+const HEIGHT = window.innerHeight
 const ASPECT_RATIO = WIDTH / HEIGHT
 
 class Board {
@@ -70,10 +71,12 @@ class Chess {
 
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(FOV, ASPECT_RATIO, 0.1, 1000)
+// const camera = new THREE.OrthographicCamera(WIDTH / -2, WIDTH / 2, HEIGHT / 2, HEIGHT / -2, 0.1, 1000)
 const raycaster = new THREE.Raycaster()
 const renderer = new THREE.WebGLRenderer({ precision: "highp", antialias: true, alpha: true })
-const loader = new GLTFLoader()
 const mouse = new THREE.Vector2()
+const controls = new OrbitControls(camera, renderer.domElement)
+const loader = new GLTFLoader()
 
 const chess = new Chess()
 
@@ -82,27 +85,32 @@ const chessBoardGroup = new THREE.Group()
 const chessOverlayGroup = new THREE.Group()
 
 const createChessBoard = () => {
-  for (let x = 0; x < 8; x++) {
-    for (let z = 0; z < 8; z++) {
-      const color = (x + z) % 2 === 0 ? 0xFFFFFF : 0x000000
+  for (let x = 0; x < 24; x++) {
+    for (let z = 0; z < 24; z++) {
+      const color =
+        x < 8 || x >= 16 || z < 8 || z >= 16 ? "rgba(255, 130, 130)" :
+        (x + z) % 2 === 0 ? "yellow" : "blue"
+
       const geom = new THREE.BoxGeometry(1, 0.2, 1)
       const mat = new THREE.MeshBasicMaterial({ color })
       const cube = new THREE.Mesh(geom, mat)
 
       chessBoardGroup.add(cube)
-      cube.position.set(x - 3.5, 0, z - 3.5)
+      // cube.position.set(x - 3.5, 0, z - 3.5)
+      cube.position.set(x - 3.5 - 8, 0, z - 3.5 - 8)
     }
   }
 
-  for (let x = 0; x < 8; x++) {
-    for (let z = 0; z < 8; z++) {
+  for (let x = 0; x < 24; x++) {
+    for (let z = 0; z < 24; z++) {
       const geom = new THREE.BoxGeometry(1, 0.2, 1)
       const mat = new THREE.MeshBasicMaterial({ color: "pink", opacity: 0.1, transparent: true })
       const cube = new THREE.Mesh(geom, mat)
-      cube.name =
+      // cube.name =
 
       chessOverlayGroup.add(cube)
-      cube.position.set(x - 3.5, 0, z - 3.5)
+      // cube.position.set(x - 3.5, 0, z - 3.5)
+      cube.position.set(x - 3.5 - 8, 0, z - 3.5 - 8)
     }
   }
 
@@ -110,42 +118,82 @@ const createChessBoard = () => {
   scene.add(chessOverlayGroup)
 }
 
-const setChessPiecePosition = (piece, x, z) => {
-  piece.position.set(x - 3.5, 0.2, z - 3.5)
+const loadChessGltfScene = () => {
+  return new Promise((resolve, reject) => {
+    loader.load("/scene.gltf", gltf => {
+      resolve(gltf)
+    }, undefined, err => reject(err))
+  })
 }
 
-const placeChessPieces = () => {
+const placeChessPieces = async () => {
+  const chessScene = await loadChessGltfScene()
+  const pieces = chessScene.scene.children[0].children[0].children[0].children[0].children
+
+  // pieces.forEach(p => chessPiecesGroup.add(p))
+  console.log(pieces)
+
   chess.map((tile, x, z) => {
     if (!tile) return
 
-    const color = tile.colour === "BLACK" ? "blue" : "pink"
-    const shape = tile.type === "PAWN" ? { width: 0.2, height: 0.5 } :
-      tile.type === "ROOK" ? { width: 0.5, height: 0.7 } :
-      tile.type === "HORSE" ? { width: 0.4, height: 0.9 } :
-      tile.type === "BISHOP" ? { width: 0.2, height: 1 } :
-      tile.type === "QUEEN" ? { width: 0.5, height: 1.3 } :
-      tile.type === "KING" ? { width: 0.4, height: 1.1 } : null
+    const modelName =
+      tile.colour === "WHITE" && tile.type === "ROOK" ? "Rook_White_0" :
+      tile.colour === "WHITE" && tile.type === "HORSE" ? "Knight_White_0" :
+      tile.colour === "WHITE" && tile.type === "BISHOP" ? "Bishop_White_0" :
+      tile.colour === "WHITE" && tile.type === "QUEEN" ? "Queen_White_0" :
+      tile.colour === "WHITE" && tile.type === "KING" ? "King_White_0" :
+      tile.colour === "WHITE" && tile.type === "PAWN" ? "Pawn001_White_0" :
+      tile.colour === "BLACK" && tile.type === "ROOK" ? "Rook002_Black_0" :
+      tile.colour === "BLACK" && tile.type === "HORSE" ? "Knight002_Black_0" :
+      tile.colour === "BLACK" && tile.type === "BISHOP" ? "Bishop002_Black_0" :
+      tile.colour === "BLACK" && tile.type === "QUEEN" ? "Queen001_Black_0" :
+      tile.colour === "BLACK" && tile.type === "KING" ? "King001_Black_0" :
+      tile.colour === "BLACK" && tile.type === "PAWN" ? "Pawn008_Black_0" :
+      null
 
-    const geom = new THREE.BoxGeometry(shape.width, shape.height, shape.width)
+    const model = pieces.find(p => p.children[0].children[0]
+      && p.children[0].children[0].name === modelName)
+
+    const piece = model.clone()
+    const color = tile.colour === "WHITE" ? "#fff" : "#000"
     const mat = new THREE.MeshBasicMaterial({ color })
-    const cube = new THREE.Mesh(geom, mat)
-    cube.name = tile.id
+    piece.children[0].children[0].material = mat
 
-    chessPiecesGroup.add(cube)
-    setChessPiecePosition(cube, x, z)
+    // const modelName =
+    //   tile.colour === "BLACK" && tile.type === "PAWN" && x === 0 ? "Pawn" :
+    //   tile.colour === "BLACK" && tile.type === "PAWN" && x === 1 ? "Pawn002" :
+    //   tile.colour === "BLACK" && tile.type === "PAWN" && x === 2 ? "Pawn003" :
+    //   tile.colour === "BLACK" && tile.type === "PAWN" && x === 3 ? "Pawn004" :
+    //   tile.colour === "BLACK" && tile.type === "PAWN" && x === 4 ? "Pawn005" :
+    //   tile.colour === "BLACK" && tile.type === "PAWN" && x === 5 ? "Pawn006" :
+    //   tile.colour === "BLACK" && tile.type === "PAWN" && x === 6 ? "Pawn007" :
+    //   tile.colour === "BLACK" && tile.type === "PAWN" && x === 7 ? "Pawn008" :
+    //   null
+
+    // const model = pieces.find(p => p.name === modelName)
+    // if (!model) return
+
+    // const color = tile.colour === "BLACK" ? "blue" : "pink"
+    // const shape = tile.type === "PAWN" ? { width: 0.2, height: 0.5 } :
+    //   tile.type === "ROOK" ? { width: 0.5, height: 0.7 } :
+    //   tile.type === "HORSE" ? { width: 0.4, height: 0.9 } :
+    //   tile.type === "BISHOP" ? { width: 0.2, height: 1 } :
+    //   tile.type === "QUEEN" ? { width: 0.5, height: 1.3 } :
+    //   tile.type === "KING" ? { width: 0.4, height: 1.1 } : null
+
+    // const geom = new THREE.BoxGeometry(shape.width, shape.height / 2, shape.width)
+    // const mat = new THREE.MeshBasicMaterial({ color })
+    // const cube = new THREE.Mesh(geom, mat)
+    piece.name = tile.id
+    piece.scale.x = 0.009
+    piece.scale.y = 0.009
+    piece.scale.z = 0.009
+
+    chessPiecesGroup.add(piece)
+    // cube.position.set(x - 3.5, 0.1 + shape.height / 4, z - 3.5)
+    piece.position.set(x - 3.5, 0.1, z - 3.5)
+    console.log(piece)
   })
-  // for (let side = 0; side < 2; side++) {
-  //   for (let x = 0; x < 8; x++) {
-  //     const color = side === 0 ? "red" : "blue"
-  //     const z = side === 0 ? 1 : 6
-  //     const geom = new THREE.BoxGeometry(0.3, 1, 0.3)
-  //     const mat = new THREE.MeshBasicMaterial({ color })
-  //     const cube = new THREE.Mesh(geom, mat)
-
-  //     chessPiecesGroup.add(cube)
-  //     cube.position.set(x - 3.5, 0.2, z - 3.5)
-  //   }
-  // }
 
   scene.add(chessPiecesGroup)
 }
@@ -162,34 +210,78 @@ const setup = async () => {
 
 let selectedPiece = null
 let clicked = false
+// let discarded = []
+
+// const renderDiscarded = () => {
+//   let discardedPosition = { x: -10, y: 0.2, z: 3 }
+
+//   discarded.forEach(d => {
+
+//   })
+// }
+
 const render = time => {
   requestAnimationFrame(render)
-
-  camera.position.x = 0
-  camera.position.y = 6
-  camera.position.z = 6
-  camera.rotation.x = Math.PI * 1.7
-
-  chess.map((tile, x, z) => {
-    if (tile === undefined) return
-
-    chessPiecesGroup.children.forEach(piece => {
-      if (piece.name === tile.id)
-        setChessPiecePosition(piece, x, z)
-    })
-  })
 
   raycaster.setFromCamera(mouse, camera)
 
   const intersects = raycaster.intersectObjects(scene.children, true)
+  // console.log(intersects)
   const overlay = intersects.find(intersect => {
     return chessOverlayGroup.children.some(c => c.uuid === intersect.object.uuid)
   })
 
-  // console.log(chessOverlayGroup.children)
-  chessOverlayGroup.children.forEach(c => c.material.opacity = 0)
-  if (overlay) overlay.object.material.opacity = 0.5
+  const hoveredPiece = overlay && chessPiecesGroup.children.find(c => c.position.x === overlay.object.position.x
+    && c.position.z === overlay.object.position.z)
 
+  // chessPiecesGroup.children.forEach(c => {
+  //   c.position.y = 0
+  // })
+
+  chessBoardGroup.children.forEach(c => {
+    c.position.y = 0
+  })
+
+  chessOverlayGroup.children.forEach(c => {
+    c.material.opacity = 0
+    c.position.y = 0
+  })
+
+  // if (hoveredPiece) hoveredPiece.position.y = 0.2
+
+  if (overlay) {
+    // const tile = chessBoardGroup.children.find(c => c.position.x === overlay.object.position.x
+    //     && c.position.z === overlay.object.position.z)
+    overlay.object.material.opacity = 0.5
+    // overlay.object.position.y = 0
+    // tile.position.y = 0.4
+
+    // if (hoveredPiece) hoveredPiece.position.y = 0.6
+  }
+
+  if (overlay && clicked) {
+    if (selectedPiece) {
+      if (hoveredPiece) chessPiecesGroup.remove(hoveredPiece)
+
+      selectedPiece.position.x = overlay.object.position.x
+      selectedPiece.position.z = overlay.object.position.z
+      selectedPiece = null
+    } else {
+      selectedPiece = chessPiecesGroup.children.find(c => c.position.x === overlay.object.position.x
+        && c.position.z === overlay.object.position.z)
+    }
+  }
+
+  if (selectedPiece) {
+    // selectedPiece.position.y = 1
+
+    if (overlay) {
+      selectedPiece.position.x = overlay.point.x
+      selectedPiece.position.z = overlay.point.z
+    }
+  }
+
+  clicked = false
   renderer.render(scene, camera)
 }
 
@@ -207,15 +299,26 @@ const click = e => {
     chess.start()
 
     createChessBoard()
-    placeChessPieces()
+    await placeChessPieces()
+
+    // const panoGeom = new THREE.SphereBufferGeometry(500, 60, 40)
+    // panoGeom.scale(-1, 1, 1)
+    // const panoTex = new THREE.TextureLoader().load("29277159034_6279f549af_o.jpg")
+    // const panoMat = new THREE.MeshBasicMaterial({ map: panoTex })
+    // const pano = new THREE.Mesh(panoGeom, panoMat)
+    // pano.rotation.y = -Math.PI / 2
+    // scene.add(pano)
 
     renderer.setSize(WIDTH, HEIGHT)
     document.body.appendChild(renderer.domElement)
 
     await setup()
-    // console.log(chessPieces)
 
-    // scene.add(chessPieces.scene)
+    camera.position.x = 0
+    camera.position.y = 8
+    camera.position.z = 8
+    controls.update()
+
     console.log(scene.children)
     render(0)
 
